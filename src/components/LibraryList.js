a98e3fa509cc6803cc350779f271475d2da82038
Deepjyoti <deepjyoti@hike.in>
2017-07-15 04:46:20 +0530

    MapStateToProps with Connect

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -3,14 +3,22 @@ import { connect } from 'react-redux';
 
 class LibraryList extends Component {
   render() {
-    return (
-
-    )
+    console.log(this.props);
+    return;
   }
 }
+
+/**
+ * [mapStateToProps take global state object inside redux store and provide them as props to LibraryList]
+ * @param  {[type]} state [description]
+ * @return {[type]}       [description]
+ */
+const mapStateToProps = state => {
+  return { libraries: state.libraries };
+};
 
 /**
  * connect is the glue between redux and react for state data excahnge
  * when connect is called it returns a function, then we call that returned function with LibraryList as parameter
  */
-export default connect()(LibraryList);
+export default connect(mapStateToProps)(LibraryList);
