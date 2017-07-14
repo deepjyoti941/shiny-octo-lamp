2ac2090e3c90241854dc17226451230db17cd33b
Deepjyoti <deepjyoti@hike.in>
2017-07-15 03:59:48 +0530

    creating first reducer

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { View } fron 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 export default class App extends Component {
   render() {
     return (
-      <View />
+      <Provider store={createStore(reducers)}>
+        <View />
+      </Provider>
     )
   }
 }
